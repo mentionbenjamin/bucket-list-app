1773c7f6ffc47f87bb68031c83dbe2ba6540d78e
Ben Robinson <mentionbenjamin@gmail.com>
2018-08-15 13:22:31 +0100

    adds request routes

--- client/src/helpers/request.js
+++ client/src/helpers/request.js
@@ -9,4 +9,20 @@ Request.prototype.get = function () {
     });
 };
 
+Request.prototype.post = function (payload) {
+  return fetch(this.rul, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then((response) => response.json());
+};
+
+Request.prototype.delete = function (id) {
+  return fetch(`${this.url}/${id}`, {
+    method: 'DELETE'
+  })
+    .then((response) => response.json());
+};
+
 module.exports = Request;
