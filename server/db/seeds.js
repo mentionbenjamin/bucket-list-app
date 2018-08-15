ebf9de50a7cfb9f9da27fc606d6203fac10964d3
Ben Robinson <mentionbenjamin@gmail.com>
2018-08-15 13:16:49 +0100

    adds seeds data

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,1 +1,14 @@
-// Seeds
+use bucket_list;
+db.dropDatabase();
+
+db.dreams.insertMany([
+  {
+    text: "Get a job"
+  },
+  {
+    text: "Consolidate learning in Pub/Sub"
+  },
+  {
+    text: "Make the div spin"
+  }
+]);
