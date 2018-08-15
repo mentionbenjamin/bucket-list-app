f3a98ece6faa9a67466a1ce60f6d3db0d21b93b7
Gary Muir <garymuir17@gmail.com>
2018-08-15 11:12:45 +0100

    adds get to request

--- client/src/helpers/request.js
+++ client/src/helpers/request.js
@@ -1,1 +1,12 @@
-// Request
+const Request = function (url) {
+  this.url = url;
+};
+
+Request.prototype.get = function () {
+  return fetch(this.url)
+    .then((response) => {
+        response.json()
+    });
+};
+
+module.exports = Request;
