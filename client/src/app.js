c1568b740ae41fc0e31d41d0c9c3bc09c079054b
Gary Muir <garymuir17@gmail.com>
2018-08-15 11:24:36 +0100

    adds bind events to select_view

--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,7 @@
-// App
+const SelectView = require('./views/select_view.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form.input-form');
+  const selectView = new SelectView(form);
+  selectView.bindEvents();
+});
--- client/src/views/select_view.js
+++ client/src/views/select_view.js
@@ -1,1 +1,17 @@
-// Select View
+const PubSub = require('../helpers/pub_sub.js');
+
+const SelectView = function (form) {
+  this.form = form;
+};
+
+SelectView.prototype.bindEvents = function () {
+  this.form.addEventListener('submit', (evt) => {
+    handleSubmit(evt);
+  });
+};
+
+function handleSubmit(evt) {
+  console.log("Banana");
+};
+
+module.exports = SelectView;
