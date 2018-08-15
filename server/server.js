2823376c452872926e93863911b3431ee4c1e4c7
Ben Robinson <mentionbenjamin@gmail.com>
2018-08-15 13:30:00 +0100

    adds server file, connecting to database

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-// Server
+const express = require('express');
+const app = express();
+const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+const bodyParser = require('body-parser');
+// config
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('bucket_list');
+    const dreamsCollection = db.collection('dreams');
+    const dreamsRouter = createRouter(dreamsCollection);
+    app.use('/api/dreams', dreamsRouter);
+  })
+  .catch(console.err);
+
+app.listen(3000, function(){
+  console.log(`Listening on port ${this.address().port}`);
+});
