8f49e228ea5a93c4117147e3a4cfd64bec715a92
Gary Muir <garymuir17@gmail.com>
2018-08-15 11:09:12 +0100

    sets up pubsub

--- client/src/helpers/pub_sub.js
+++ client/src/helpers/pub_sub.js
@@ -1,1 +1,14 @@
-// Pub Sub
+const PubSub = {
+  publish: function (channel, payload) {
+    const event = new CustomEvent(channel, {
+      detail: payload
+    });
+    document.dispatchEvent(event);
+  },
+
+  subscribe: function (channel, callback) {
+    document.addEventListener(channel, callback);
+  }
+};
+
+module.exports = PubSub;
